095f7cd6a1b6da0daf46e33f3f48ca80adfa969f
“cristopher” <curibe26@aoiths.org>
2025-11-07 13:38:45 -0500

    daa

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 function getNumber(id) {
   return parseFloat(document.getElementById(id).value) || 0;
 }
+function showResult(id, result) { 
+     document.getElementById(id).textContent = message;
+}
