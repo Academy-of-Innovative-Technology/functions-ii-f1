03b3fe189cf9b0457411045741cdcd9fe40ab2c7
“cristopher” <curibe26@aoiths.org>
2025-11-07 13:37:44 -0500

    we

--- script.js
+++ script.js
@@ -2,10 +2,3 @@
 function getNumber(id) {
   return parseFloat(document.getElementById(id).value) || 0;
 }
-
-function showResult(id, message) {
-  document.getElementById(id).textContent = message;
-}
-
-
-
